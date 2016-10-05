6bb5266fde9953ed61ae2537a3725a9ef3909b27
OmniP <412726700@qq.com>
2016-10-05 13:13:46 +0800

    add fonts and login

---
+++ src/app/login.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by pzc on 16-10-2.
+ */
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'selector',
+  templateUrl: 'feature.component.html'
+})
+export class FeatureComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
