395122d665dd9b852685ce83a9cfafdf9f5882d8
OmniP <412726700@qq.com>
2016-10-08 09:12:08 +0800

    update

--- src/app/topic.component.ts
+++ src/app/topic.component.ts
@@ -2,11 +2,11 @@
  * Created by panzhichao on 2016/10/1.
  */
 
-import {Component, OnInit} from "@angular/core";
-import {Http} from "@angular/http";
-import {ActivatedRoute, Params} from '@angular/router';
-import {Location}               from '@angular/common';
-import * as moment from 'moment'
+import { Component, OnInit } from "@angular/core";
+import { Http } from "@angular/http";
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import * as moment from 'moment';
 
 import 'rxjs/add/operator/toPromise';
 
@@ -14,29 +14,31 @@ import 'rxjs/add/operator/toPromise';
   templateUrl: 'topic.component.html'
 })
 export class TopicComponent implements OnInit {
-  topic:any;
-  id:string;
-  loading:boolean = true;
+  topic: any;
+  id: string;
+  loading: boolean = true;
   moment = moment;
-
-  constructor(private http:Http, private route:ActivatedRoute, private location:Location) {
+  md=markdownit({html: true, linkify: false, typographer: true,breaks:true});
+  constructor(private http: Http, private route: ActivatedRoute, private location: Location) {
 
   }
 
   ngOnInit() {
-    this.route.params.forEach((params:Params) => {
+    this.route.params.forEach((params: Params) => {
       this.id = params['id'];
-      this.fetchData().then(data=>this.topic = data)
+      this.fetchData().then(data => this.topic = data)
     });
   }
 
 
   fetchData() {
+   
     this.loading = true;
-    return this.http.get(`https://cnodejs.org/api/v1/topic/${this.id}`)
+    return this.http.get(`https://cnodejs.org/api/v1/topic/${this.id}?mdrender=false`)
+      .timeout(3000, new Error('timeout exceeded'))
       .toPromise()
-      .then(res=>res.json())
-      .then(res=> {
+      .then(res => res.json())
+      .then(res => {
         this.loading = false;
         return res.data
       })
